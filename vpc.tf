e5dd2dbcb256dc846e1085978f7395d45f7f6dad
ashgun2009-ship-it <ashgun2009@gmail.com>
2026-06-03 23:25:58 +0300

    Add files via upload

--- vpc.tf
+++ vpc.tf
@@ -49,9 +49,8 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# Налаштовуємо стандартну (Main) таблицю маршрутів, яку AWS створив сам
-resource "aws_default_route_table" "public_rt" {
-  default_route_table_id = aws_vpc.main.default_route_table_id
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -59,21 +58,21 @@ resource "aws_default_route_table" "public_rt" {
   }
 
   tags = {
-    Name = "cmtr-o3e0v1ec-01-route-table"
+    Name = var.routing_table
   }
 }
 
 resource "aws_route_table_association" "assoc1" {
   subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_default_route_table.public_rt.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "assoc2" {
   subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_default_route_table.public_rt.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "assoc3" {
   subnet_id      = aws_subnet.subnet3.id
-  route_table_id = aws_default_route_table.public_rt.id
+  route_table_id = aws_route_table.public_rt.id
 }
