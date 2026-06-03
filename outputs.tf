2403068c146993edb676f22199dab6fc1b8a097c
ashgun2009-ship-it <ashgun2009@gmail.com>
2026-06-03 23:05:04 +0300

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,23 @@
 output "vpc_id" {
   value       = aws_vpc.main.id
-  description = "Ідентифікатор створеної VPC"
+  description = "The ID of the created VPC."
 }
 
-output "subnet_ids" {
-  # Виведемо ID всіх трьох підмереж списком
-  value       = [aws_subnet.subnet1.id, aws_subnet.subnet2.id, aws_subnet.subnet3.id]
-  description = "Список ІД створених публічних підмереж"
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.subnet1.id,
+    aws_subnet.subnet2.id,
+    aws_subnet.subnet3.id
+  ]
+  description = "List of IDs of the created public subnets."
+}
+
+output "internet_gateway_id" {
+  value       = aws_internet_gateway.igw.id
+  description = "The ID of the attached Internet Gateway."
+}
+
+output "route_table_id" {
+  value       = aws_route_table.public_rt.id
+  description = "The ID of the public Route Table."
 }
